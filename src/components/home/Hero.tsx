65d324a1c9b71fd80f0be2f72e72aa42d10f7bae
Neverm1ndEZ <mihirsuman1@gmail.com>
2025-03-22 14:37:40 +0530

    fixed mobile responsiveness

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -7,18 +7,18 @@ export default function Hero() {
 				<h1 className="text-4xl md:text-6xl lg:text-7xl uppercase font-bold text-[#f5f5f0] mb-4 shadow-[0_4px_8px_rgba(0,0,0,0.5)]">
 					Property Solutions
 				</h1>
-				<h2 className="text-lg md:text-xl lg:text-2xl uppercase font-semibold text-[#f5f5f0] mb-4 underline shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
+				<h2 className="text-base md:text-xl lg:text-2xl uppercase font-semibold text-[#f5f5f0] mb-4 underline shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
 					Property Dealer in Greater Noida
 				</h2>
-				<h3 className="text-lg md:text-2xl lg:text-3xl uppercase font-semibold text-[#f5f5f0] max-w-5xl mx-auto shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
+				<h3 className="text-[14px] md:text-2xl lg:text-3xl uppercase font-semibold text-[#f5f5f0] max-w-5xl mx-auto shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
 					Industrial plots | Residential plots | Factory | Warehouse
-					<br />
-					Greater Noida | Yamuna Expressway | Sikandrabad
-					<br />
-					<span className="text-base md:text-lg lg:text-xl uppercase font-semibold text-[#f5f5f0] mt-8 underline shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
-						Best Property Dealer for Yamuna Expressway Authority Plots
-					</span>
 				</h3>
+				<h3 className="text-[14px] md:text-2xl lg:text-3xl uppercase font-semibold text-[#f5f5f0] max-w-5xl mx-auto shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
+					Greater Noida | Yamuna Expressway | Sikandrabad
+				</h3>
+				<span className="text-base md:text-lg lg:text-xl uppercase font-semibold text-[#f5f5f0] mt-8 underline shadow-[0_2px_4px_rgba(0,0,0,0.5)]">
+					Best Property Dealer for Yamuna Expressway Authority Plots
+				</span>
 			</div>
 		</div>
 	);
